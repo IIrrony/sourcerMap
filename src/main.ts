40d6d7ac9d2e9880c6289cc92c6b847530af9e8e
ren yyyyfffff <1559902303@qq.com>
2025-04-11 12:36:51 +0800

    feat:映射源码

--- src/main.ts
+++ src/main.ts
@@ -3,22 +3,33 @@ import ErrorStackParser from 'error-stack-parser'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import 'element-plus/theme-chalk/index.css'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
 
-import { findCodeBySourceMap } from './utils'
+// import { findCodeBySourceMap } from './utils'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
-app.config.errorHandler = (err, vm) => {
+app.config.errorHandler = (err: any, vm) => {
   // Handle error
   const errorStack = ErrorStackParser.parse(err as Error)
-  findCodeBySourceMap(errorStack[0])
-  console.error('Error stack:', errorStack)
+  const jsError = {
+    stack_frames: errorStack,
+    message: err.message,
+    stack: err.stack,
+    err_name: err.name,
+  }
+  vm!.$message.error(`你触发了一个错误：${err.name}`)
+  localStorage.setItem('jsErrorList', JSON.stringify(jsError))
+  // findCodeBySourceMap(errorStack[0])
+  // console.error('Error stack:', errorStack)
   // console.error('Error:', err)
   // console.error('Component:', vm)
 }
